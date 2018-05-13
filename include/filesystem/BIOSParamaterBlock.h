c6df2861276deaddd01e91067a1974f17ba2ff71
Ondřej Langr <lango@doomsdaymachine.DOMA>
2018-05-13 22:49:35 +0200

    Boot WIP

---
+++ include/filesystem/BIOSParamaterBlock.h
@@ -0,0 +1,8 @@
+//
+// Created by lango on 5/1/18.
+//
+
+#ifndef PLUTO_BIOSPARAMATERBLOCK_H
+#define PLUTO_BIOSPARAMATERBLOCK_H
+
+#endif //PLUTO_BIOSPARAMATERBLOCK_H
---
+++ include/filesystem/BootSector.h
@@ -0,0 +1,8 @@
+//
+// Created by lango on 5/1/18.
+//
+
+#ifndef PLUTO_BOOTSECTOR_H
+#define PLUTO_BOOTSECTOR_H
+
+#endif //PLUTO_BOOTSECTOR_H
